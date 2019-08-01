0ce7052a0fdeab0935d76288a55491ceaee6b997
Bruno <bspolidorio@gmail.com>
2019-08-01 20:12:11 +0100

    deploy

--- src/datasource/Firebase.js
+++ src/datasource/Firebase.js
@@ -2,13 +2,13 @@ import app from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGQBzq1eUpPu5GqoH5_9PUTcMxohb86Ms",
-  authDomain: "jaky-counter.firebaseapp.com",
-  databaseURL: "https://jaky-counter.firebaseio.com",
-  projectId: "jaky-counter",
-  storageBucket: "",
-  messagingSenderId: "658310962156",
-  appId: "1:658310962156:web:6ffe358ce5d4559b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
